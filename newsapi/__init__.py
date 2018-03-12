457197d3e2f82bf650345cec5238a9a89f783448
Tom Faulkner <tomfaulkner@gmail.com>
2018-03-12 18:30:54 -0500

    rename import in init

--- newsapi/__init__.py
+++ newsapi/__init__.py
@@ -1,4 +1,4 @@
 from newsapi.client import NewsApi
 from newsapi.constants import COUNTRIES, CATEGORIES, LANGUAGES
 
-__all__ = ['NewsApiClient', 'COUNTRIES', 'CATEGORIES', 'LANGUAGES']
+__all__ = ['NewsApi', 'COUNTRIES', 'CATEGORIES', 'LANGUAGES']
